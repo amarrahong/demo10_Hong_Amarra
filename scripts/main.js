a255933fd544bc244e88917215d59555a03cae55
Amarra Hong <amarrahong@gmail.com>
2022-03-22 09:41:13 -0700

    Created eachHike and filter customizations

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,20 @@
+var currentUser;
+firebase.auth().onAuthStateChanged(user => {
+    if (user) {
+        currentUser = db.collection("users").doc(user.uid);   //global
+        console.log(currentUser);
+
+        // the following functions are always called when someone is logged in
+        read_display_Quote();
+        insertName();
+        populateCardsDynamically();
+    } else {
+        // No user is signed in.
+        console.log("No user is signed in");
+        window.location.href = "login.html";
+    }
+});
+
 function read_display_Quote() {
     //console.log("inside the function")
 
@@ -36,6 +53,8 @@ function populateCardsDynamically() {
     let hikeCardGroup = document.getElementById("hikeCardGroup");
 
     db.collection("Hikes")
+        .orderBy("duration")
+        .limit(2)
         .get()
         .then((allHikes) => {
             allHikes.forEach((doc) => {
@@ -43,9 +62,19 @@ function populateCardsDynamically() {
                 var hikeID = doc.data().id; //gets the unique ID field
                 var hikeLength = doc.data().length; //gets the length field
                 let testHikeCard = hikeCardTemplate.content.cloneNode(true);
-                testHikeCard.querySelector(".card-title").innerHTML = hikeName;
-                testHikeCard.querySelector(".card-length").innerHTML = hikeLength;
+                testHikeCard.querySelector(".card-title").innerHTML = hikeName;               
+                //NEW LINE: update to display length, duration, last updated
+                testHikeCard.querySelector('.card-length').innerHTML = 
+                "Length: " + doc.data().length + " km <br>" +
+                "Duration: " + doc.data().duration + "min <br>" +
+                "Last updated: " + doc.data().last_updated.toDate(); 
                 testHikeCard.querySelector("a").onclick = () => setHikeData(hikeID);
+                //this line sets the id attribute for the <i> tag in the format of "save-hikdID" 
+                //so later we know which hike to bookmark based on which hike was clicked
+                testHikeCard.querySelector('i').id = 'save-' + hikeID;
+                // this line will call a function to save the hikes to the user's document             
+                testHikeCard.querySelector('i').onclick = () => saveBookmark(hikeID);
+                testHikeCard.querySelector('.read-more').href = "eachHike.html?hikeName="+hikeName +"&id=" + hikeID;
                 testHikeCard.querySelector("img").src = `./images/${hikeID}.jpg`;
                 hikeCardGroup.appendChild(testHikeCard);
             });
@@ -63,8 +92,9 @@ function writeHikes() {
         city: "Burnaby",
         province: "BC",
         level: "easy",
-        length: "10 km",
-        length_time: "2h 33mm"
+        length: 10,          //number value
+        duration: 60,     //number value
+        last_updated: firebase.firestore.FieldValue.serverTimestamp()  //current system time
     });
     hikesRef.add({
         id: "AM01",
@@ -72,20 +102,41 @@ function writeHikes() {
         city: "Anmore",
         province: "BC",
         level: "moderate",
-        length: "10.5 km",
-        length_time: "3h 17mm"
+        length: 10.5,      //number value
+        duration: 80,   //number value
+        last_updated: firebase.firestore.Timestamp.fromDate(new Date("March 10, 2022"))
     });
     hikesRef.add({
         id: "NV01",
-        name: "Mount Seymoure Trail", //replace with your own city?
+        name: "Mount Seymour Trail", //replace with your own city?
         city: "North Vancouver",
         province: "BC",
         level: "hard",
-        length: "8.2 km",
-        length_time: "3h 20m"
+        length: 8.2,        //number value
+        duration: 120,   //number value
+        last_updated: firebase.firestore.Timestamp.fromDate(new Date("January 1, 2022"))
     });
 }
 
 function setHikeData(id){
     localStorage.setItem ('hikeID', id);
+}
+
+//-----------------------------------------------------------------------------
+// This function is called whenever the user clicks on the "bookmark" icon.
+// It adds the hike to the "bookmarks" array
+// Then it will change the bookmark icon from the hollow to the solid version. 
+//-----------------------------------------------------------------------------
+function saveBookmark(hikeID) {
+    currentUser.set({
+            bookmarks: firebase.firestore.FieldValue.arrayUnion(hikeID)
+        }, {
+            merge: true
+        })
+        .then(function () {
+            console.log("bookmark has been saved for: " + currentUser);
+            var iconID = 'save-' + hikeID;
+            //console.log(iconID);
+            document.getElementById(iconID).innerText = 'bookmark';
+        });
 }
